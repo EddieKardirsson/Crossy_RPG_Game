7db3d7279004489ac63a587bcdbea7de3751396c
Edison Kardirsson <ed.kardirsson@hotmail.com>
2023-12-18 16:59:18 +0100

    Game Window and Loop
    Game window is set and the core program loop is configured

--- main.py
+++ main.py
@@ -2,7 +2,32 @@ import pygame
 
 pygame.init()
 
-# TODO: Game code
+resolution_4k = (3840, 2160)
+resolution_2k = (2560, 1440)
+resolution_1080p = (1920, 1080)
+resolution_720p = (1280, 720)
+
+# change resolution/screen size with this variable
+current_display_size = resolution_2k
+
+white_colour = (255, 255, 255)
+black_colour = (0, 0, 0)  # using black colour background initially because it is easier to the eye
+
+game_window = pygame.display.set_mode(current_display_size)
+
+clock = pygame.time.Clock();
+
+while True:
+
+    # Handle events
+
+    # Execute logic
+
+    # Update display
+    game_window.fill(black_colour)
+    pygame.display.update()
+
+    clock.tick(60)
 
 pygame.quit()
 quit()
