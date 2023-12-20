4657ce5f479bedf9164361cb97ff400fcf5aeaf9
Edison Kardirsson <ed.kardirsson@hotmail.com>
2023-12-20 14:31:53 +0100

    Drawing the background image and center it.

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ resolution_1080p = (1920, 1080)
 resolution_720p = (1280, 720)
 
 # change resolution/screen size with this variable
-current_display_size = resolution_2k
+current_display_size = resolution_1080p
+center_position = (current_display_size[0]/2, current_display_size[1]/2)
 
 white_colour = (255, 255, 255)
 black_colour = (0, 0, 0)  # using black colour background initially because it is easier to the eye
@@ -16,6 +17,13 @@ black_colour = (0, 0, 0)  # using black colour background initially because it i
 game_window = pygame.display.set_mode(current_display_size)
 
 clock = pygame.time.Clock()
+
+# load the background image to a variable
+background_image = pygame.image.load("Assets/background.png")
+# Scale the background image with the height value (lowest scaling value)
+background = pygame.transform.scale(background_image, (current_display_size[1], current_display_size[1]))
+# center factor to position the image to the middle.
+BACKGROUND_CENTER_POS = 0.22     # After a little trial and error, this constant works perfect to center the X-position
 
 
 def run_game_loop():
@@ -31,6 +39,7 @@ def run_game_loop():
 
         # Update display
         game_window.fill(black_colour)
+        game_window.blit(background, (current_display_size[0]*BACKGROUND_CENTER_POS, 0))
         pygame.display.update()
 
         clock.tick(60)
