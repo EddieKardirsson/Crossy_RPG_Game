35def52ea7e31e6a1f16ba0a3bfb8ffdbdf81a80
Edison Kardirsson <ed.kardirsson@hotmail.com>
2023-12-20 15:11:53 +0100

    Treasure added and also made scalable to the resolution without messing up with draw size or positioning.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ resolution_1080p = (1920, 1080)
 resolution_720p = (1280, 720)
 
 # change resolution/screen size with this variable
-current_display_size = resolution_1080p
+current_display_size = resolution_2k
 center_position = (current_display_size[0]/2, current_display_size[1]/2)
 
 white_colour = (255, 255, 255)
@@ -25,6 +25,14 @@ background = pygame.transform.scale(background_image, (current_display_size[1], 
 # center factor to position the image to the middle.
 BACKGROUND_CENTER_POS = 0.22     # After a little trial and error, this constant works perfect to center the X-position
 
+OBJECT_SCALE_FACTOR = current_display_size[1] * 0.004
+
+treasure_image = pygame.image.load("Assets/treasure.png")
+treasure = pygame.transform.scale(treasure_image, (
+    treasure_image.get_width() * OBJECT_SCALE_FACTOR,
+    treasure_image.get_height() * OBJECT_SCALE_FACTOR))
+TREASURE_POS = (current_display_size[0]*0.485, current_display_size[1]*0.08)
+
 
 def run_game_loop():
     while True:
@@ -40,6 +48,7 @@ def run_game_loop():
         # Update display
         game_window.fill(black_colour)
         game_window.blit(background, (current_display_size[0]*BACKGROUND_CENTER_POS, 0))
+        game_window.blit(treasure, TREASURE_POS)
         pygame.display.update()
 
         clock.tick(60)
